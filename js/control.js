de50a500036fd77f872a4f5f28ddcdf6b4949163
Espedito Lisboa Neto <77476947+Espedito-tabajara@users.noreply.github.com>
2023-07-19 23:25:02 -0300

    Correção de bugs

--- js/control.js
+++ js/control.js
@@ -7,30 +7,16 @@ let cont = 0;
 
 function actives(id){
     document.getElementById(id).innerHTML = "radio_button_checked";
-    document.getElementById(id).style.color = "blue";
-    
-    coati = document.getElementById(`container-bx${cont}`);
-    coati.style.backgroundColor = "#9FFA95";
-
-    
-    
-    
-    
+    document.getElementById(id).style.color = "blue";   
 }
 
-function backG(id){
-    id = document.getElementById(id);
-    id.style.color = "#B7AFB7";
-    id.style.textDecoration = "line-through";
-    
-}
 
 function addtext(){
     let valorImput = imput1.value;
     if ( (valorImput !== "") && (valorImput !== null) && (valorImput  !== undefined)){
         ++cont;
         let newBloco = `
-        <div id="container-bx${cont}" class = "container">
+        <div id="container-bx${cont}" class = "container" onclick= (marcarTarefa("container-bx${cont}"))>
             <div id="check-bx${cont}" class = "check-bx"{>
                 
                     <span class="material-symbols-outlined" id="circle${cont}" onclick=(actives("circle${cont}"))>
@@ -38,8 +24,8 @@ function addtext(){
                     </span>
                 
             </div>
-            <div id="text-${cont}" class="text-02" onclick=(backG(text-${cont}))>
-                <p id="box-output">${imput1.value}</p>
+            <div id="text-${cont}" class="text-02" onclick=(actives("circle${cont}"))>
+            <p id="box-output${cont}" class="box-output">${imput1.value}</p>
             </div>
             <div id="but-del" onclick=(delet("container-bx${cont}"))>
                 <span id="lixeira" class="material-symbols-outlined">
@@ -54,6 +40,17 @@ function addtext(){
     }
     
 
+}
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    item.style.color = '#B7AFB7';
+    item.style.backgroundColor = '#9FFA95';
+    var paragrafo = item.querySelector('p.box-output');
+    paragrafo.style.textDecoration = 'line-through';
+    item.parentNode.appendChild(item)
+   
+    
+     
 }
 
 
